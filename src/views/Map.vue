a6dfa00215072243996c5e696ac365f44596293a
lirt <540491950@qq.com>
2020-12-26 23:01:42 +0800

    项目初始化

---
+++ src/views/Map.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="con">
+  </div>
+</template>
+
+<script>
+window.CESIUM_BASE_URL = '/Cesium/'
+import * as Cesium from 'cesium'
+import 'cesium/Build/Cesium/Widgets/widgets.css'
+
+export default {
+  name: 'Map',
+  data() {
+    return {
+      viewer: null
+    }
+  },
+  mounted() {
+    this.initMap()
+  },
+  methods: {
+    initMap() {
+      this.viewer = new Cesium.Viewer('con')
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+#con {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
